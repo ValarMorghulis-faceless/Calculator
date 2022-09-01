de4898b53641a7d5b88ca518da4e6126529dd50e
Giorgi samkhar <padregiorgi777@gmail.com>
2022-09-02 03:20:47 +0400

    Calculator

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Giorgi Samkharadze on 02.09.22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
