4c8920e573667e44032bca618f4032a255e07649
Giorgi samkhar <padregiorgi777@gmail.com>
2022-09-02 03:56:27 +0400

    made code work and added comments

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+//CODEREV: enum for calculator buttons
 enum CalcButton: String {
     case one = "1"
     case two = "2"
@@ -27,7 +27,7 @@ enum CalcButton: String {
     case decimal = "."
     case percent = "%"
     case negative = "-/+"
-    
+    //CODEREV: button colors for different buttons
     var buttonColor: Color {
         switch self {
         case .add, .substract, .multiply, .divide, .equal:
@@ -40,7 +40,7 @@ enum CalcButton: String {
         }
     }
 }
-
+//CODEREV: enum for operation logic
 enum Operation {
     case add, substract, multiply, divide, none
     
@@ -51,7 +51,7 @@ struct ContentView: View {
     @State var value = "0"
     @State var runningNumber = 0
     @State var currentOperation: Operation = .none
-    
+    //CODEREV: buttons arr to use in logic and View
     let buttons: [[CalcButton]] = [
         [.clear, .negative, .percent, .divide],
         [.seven, .eight, .nine, .multiply],
@@ -61,10 +61,12 @@ struct ContentView: View {
     ]
     
     var body: some View {
+        //CODEREV: ZStack for the black color
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
             VStack {
                 Spacer()
+                //HStack for the text display
                 HStack {
                     Spacer()
                 Text(value)
@@ -73,6 +75,7 @@ struct ContentView: View {
                     .foregroundColor(.white)
                 }
                 .padding()
+                //CODEREV: VStack two forloops for button tier
                 ForEach(buttons, id: \.self) { row in
                     HStack(spacing: 12) {
                         ForEach(row, id: \.self) { item in
@@ -94,21 +97,23 @@ struct ContentView: View {
     }
         
 }
+    //CODEREV: actual function to tap a button, we are basicly switching tap a button and if it is operation type we hold on type of operation as well as current value, cuz thats the value we are gonna apply operation to. in case of adding multiplying etc..
     func didTap(button: CalcButton) {
         switch button {
         case .add, .substract, .multiply ,.divide, .equal:
             if button == .add {
                 self.currentOperation = .add
-                self.runningNumber += Int(self.value) ?? 0
+                self.runningNumber = Int(self.value) ?? 0
             } else if button == .substract {
                 self.currentOperation = .substract
-                self.runningNumber += Int(self.value) ?? 0
+                self.runningNumber = Int(self.value) ?? 0
             }else if button == .multiply {
                 self.currentOperation = .multiply
-                self.runningNumber += Int(self.value) ?? 0
+                self.runningNumber = Int(self.value) ?? 0
             }else if button == .divide {
                 self.currentOperation = .divide
-                self.runningNumber += Int(self.value) ?? 0
+                self.runningNumber = Int(self.value) ?? 0
+                    // CODEREV: and if user hits equal grab the current value and apply operation to it
             }else if button == .equal {
                
                 let runningValue = self.runningNumber
@@ -125,10 +130,12 @@ struct ContentView: View {
             if button != .equal {
                 self.value = "0"
             }
+            //CODEREV: if user hits clear we clear up the value
         case .clear:
             self.value = "0"
         case .decimal, .negative, .percent:
             break
+            // CODEREV: default is all the numbers, if the numbers get hit and current value is zero asign value whatever the input number is, otherwise we tac on numbers
         default:
             let number = button.rawValue
             if self.value == "0" {
@@ -139,7 +146,7 @@ struct ContentView: View {
             }
         }
     }
-    
+    // CODEREV: button modifiers
     func buttonWidth(item: CalcButton) -> CGFloat {
         if item == .zero {
             return ((UIScreen.main.bounds.width - (4*12)) / 4) * 2
